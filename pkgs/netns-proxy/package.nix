898ce2046d8066e21713ca2265f56e1ccdd74275
Lu Wang <rebmit@rebmit.moe>
2024-12-08 20:18:37 +0800

    pkgs/ranet: init at 0.11.0

--- pkgs/netns-proxy/package.nix
+++ pkgs/netns-proxy/package.nix
@@ -1,17 +1,28 @@
-{ rustPlatform, fetchFromGitHub, ... }:
+{
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+  ...
+}:
 
 rustPlatform.buildRustPackage rec {
-  name = "netns-proxy";
+  pname = "netns-proxy";
+  version = "unstable-2023-09-17";
 
   src = fetchFromGitHub {
     owner = "fooker";
     repo = "netns-proxy";
     rev = "6d9ccbfde4375cd614735ea5f6ee5aba2b6cfd2b";
-    fetchSubmodules = false;
     sha256 = "sha256-N+my6cTuA7yNoYxocpRiLNcy7OwrJLvO2cGLJGv8a/I=";
   };
 
   cargoLock = {
     lockFile = src + /Cargo.lock;
+  };
+
+  meta = with lib; {
+    description = "A simple and slim proxy to forward ports from and into linux network namespaces";
+    homepage = "https://github.com/fooker/netns-proxy";
+    license = licenses.mit;
   };
 }
