6c418a0357f8b24602dbbcf611c811fda9fcf380
Lu Wang <rebmit@rebmit.moe>
2025-08-04 22:57:33 +0800

    pkgs/ranet: remove useFetchCargoVendor usage

--- pkgs/ranet/package.nix
+++ pkgs/ranet/package.nix
@@ -15,7 +15,6 @@ rustPlatform.buildRustPackage rec {
     sha256 = "sha256-xHdIuT/j35amoTjc4/s0uFFVLiM7BmLIEaZLGSa3QGU";
   };
 
-  useFetchCargoVendor = true;
   cargoHash = "sha256-Qd7Hy/Mq2XihTB7RHQYjRKjaM5eigLxL+MpVyUmBozk=";
 
   checkFlags = [
