a16d4a682b521eb09f9fb415c390944026eb5cec
Lu Wang <rebmit@rebmit.moe>
2025-10-14 21:16:01 +0800

    netns/hosts: implement dns domainname

--- modules/features/netns/config/hosts.nix
+++ modules/features/netns/config/hosts.nix
@@ -1,5 +1,6 @@
 # Portions of this file are sourced from
 # https://github.com/NixOS/nixpkgs/blob/0a53886700520c494906ab04a4f9b39d61bfdfb9/nixos/modules/config/networking.nix (MIT License)
+# https://github.com/NixOS/nixpkgs/blob/0a53886700520c494906ab04a4f9b39d61bfdfb9/nixos/modules/tasks/network-interfaces.nix (MIT License)
 { lib, ... }:
 let
   inherit (lib) types;
@@ -11,11 +12,9 @@ let
 in
 {
   flake.modules.nixos.netns =
-    { config, ... }:
+    { config, ... }@host:
     let
       inherit (config.passthru.netns.lib) mkNetnsOption;
-
-      inherit (config.networking) hostName domain;
     in
     {
       options.netns = mkNetnsOption (
@@ -24,6 +23,38 @@ in
           _file = ./hosts.nix;
 
           options = {
+            hostName = mkOption {
+              type = types.strMatching "^$|^[[:alnum:]]([[:alnum:]_-]{0,61}[[:alnum:]])?$";
+              default = host.config.networking.hostName;
+              readOnly = true;
+              description = ''
+                The machine’s hostname.  Must match `networking.hostName` exactly
+                because we share the UTS namespace.
+              '';
+            };
+            domain = mkOption {
+              type = types.nullOr types.str;
+              default = null;
+              description = ''
+                The system domain name for this network namespace.
+              '';
+            };
+            fqdn = mkOption {
+              type = types.str;
+              default =
+                if (config.hostName != "" && config.domain != null) then
+                  "${config.hostName}.${config.domain}"
+                else
+                  throw ''
+                    The FQDN is required but cannot be determined from `hostName`
+                    and `domain`.  Please ensure these options are set properly
+                    or set `fqdn` directly.
+                  '';
+              description = ''
+                The fully qualified domain name (FQDN) of this host within this
+                network namespace.
+              '';
+            };
             hosts = mkOption {
               type = types.attrsOf (types.listOf types.str);
               default = { };
@@ -43,6 +74,7 @@ in
           config = mkIf config.enable {
             hosts =
               let
+                inherit (config) hostName domain;
                 hostnames =
                   optional (hostName != "" && domain != null) "${hostName}.${domain}"
                   ++ optional (hostName != "") hostName;
