f65b957af5ceb5f11d0ba4be0e5c5237b51560e6
Lu Wang <rebmit@rebmit.moe>
2025-10-29 01:34:44 +0800

    treewide: stop exposing unnecessary flake modules

--- modules/meta.nix
+++ modules/meta.nix
@@ -3,22 +3,18 @@ let
   inherit (lib.types) submodule;
   inherit (lib.options) mkOption;
   inherit (selfLib.types) mkStructuredType;
-
-  metaModule = _: {
-    options.flake.meta = mkOption {
-      type = submodule {
-        freeformType = mkStructuredType { typeName = "meta"; };
-      };
-      description = ''
-        A set of freeform attributes for flake internal usage.
-      '';
-    };
-  };
 in
 {
-  imports = [ metaModule ];
+  options.flake.meta = mkOption {
+    type = submodule {
+      freeformType = mkStructuredType { typeName = "meta"; };
+    };
+    description = ''
+      A set of freeform attributes for flake internal usage.
+    '';
+  };
 
-  flake.modules.flake.meta = metaModule;
-
-  flake.meta.uri = "github:rebmit/nix-exprs";
+  config = {
+    flake.meta.uri = "github:rebmit/nix-exprs";
+  };
 }
--- modules/systems.nix
+++ modules/systems.nix
@@ -1,14 +1,7 @@
-let
-  systemsModule = _: {
-    systems = [
-      "x86_64-linux"
-      "aarch64-linux"
-      "aarch64-darwin"
-    ];
-  };
-in
 {
-  imports = [ systemsModule ];
-
-  flake.modules.flake.systems = systemsModule;
+  systems = [
+    "x86_64-linux"
+    "aarch64-linux"
+    "aarch64-darwin"
+  ];
 }
