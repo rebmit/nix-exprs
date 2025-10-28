8d83ce2f1a7e8a73d68a98619f6b93abffd63974
Lu Wang <rebmit@rebmit.moe>
2025-10-28 23:32:00 +0800

    nixpkgs: cleanup

--- modules/nixpkgs.nix
+++ modules/nixpkgs.nix
@@ -10,7 +10,8 @@
 }:
 let
   inherit (lib) types;
-  inherit (lib.lists) foldl any;
+  inherit (lib.lists) foldl any elem;
+  inherit (lib.strings) getName;
   inherit (lib.options) mkOption;
   inherit (flake-parts-lib) mkPerSystemOption;
 
@@ -88,52 +89,59 @@ let
     );
   };
 
-  nixpkgsAllowedPredicatesModule = _: {
+  nixpkgsPredicatesModule = _: {
     options.perSystem = mkPerSystemOption (
       { config, ... }:
       let
-        mkPredicatesOption =
-          description:
-          mkOption {
-            type = (types.functionTo types.bool) // {
-              merge =
-                _loc: defs:
-                let
-                  funcs = map (d: d.value) defs;
-                in
-                foldl (acc: f: acc ++ [ f ]) [ ] funcs;
-            };
-            default = _p: false;
-            inherit description;
+        predicateOption = mkOption {
+          type = (types.functionTo types.bool) // {
+            merge =
+              _loc: defs:
+              let
+                funcs = map (d: d.value) defs;
+              in
+              foldl (acc: f: acc ++ [ f ]) [ ] funcs;
           };
+          default = _: false;
+        };
 
-        cfg = config.nixpkgs.allowedPredicates;
+        cfg = config.nixpkgs.predicates;
       in
       {
-        options.nixpkgs.allowedPredicates = {
-          unfree = mkPredicatesOption ''
-            List of predicates deciding which packages are allowed even if unfree.
-            Only effective when `allowUnfree = false`.
-          '';
-          nonSource = mkPredicatesOption ''
-            List of predicates deciding which packages are allowed even if they are not built from source.
-            Only effective when `allowNonSource = false`.
-          '';
-          broken = mkPredicatesOption ''
-            List of predicates deciding which packages are allowed even if marked as broken.
-            Only effective when `allowBroken = false`.
-          '';
-          insecure = mkPredicatesOption ''
-            List of predicates deciding which packages are allowed even if marked as insecure.
-          '';
+        _file = ./nixpkgs.nix;
+
+        options.nixpkgs.predicates = {
+          allowUnfree = predicateOption // {
+            description = ''
+              List of predicates deciding which packages are allowed even if unfree.
+              Only effective when `allowUnfree = false`.
+            '';
+          };
+          allowNonSource = predicateOption // {
+            description = ''
+              List of predicates deciding which packages are allowed even if they are not built from source.
+              Only effective when `allowNonSource = false`.
+            '';
+          };
+          allowBroken = predicateOption // {
+            description = ''
+              List of predicates deciding which packages are allowed even if marked as broken.
+              Only effective when `allowBroken = false`.
+            '';
+          };
+          allowInsecure = predicateOption // {
+            description = ''
+              List of predicates deciding which packages are allowed even if marked as insecure.
+            '';
+          };
         };
 
         config = {
           nixpkgs.config = {
-            allowUnfreePredicate = p: any (f: f p) cfg.unfree;
-            allowNonSourcePredicate = p: any (f: f p) cfg.nonSource;
-            allowBrokenPredicate = p: any (f: f p) cfg.broken;
-            allowInsecurePredicate = p: any (f: f p) cfg.insecure;
+            allowUnfreePredicate = p: any (f: f p) cfg.allowUnfree;
+            allowNonSourcePredicate = p: any (f: f p) cfg.allowNonSource;
+            allowBrokenPredicate = p: any (f: f p) cfg.allowBroken;
+            allowInsecurePredicate = p: any (f: f p) cfg.allowInsecure;
           };
         };
       }
@@ -143,17 +151,34 @@ in
 {
   imports = [
     nixpkgsModule
-    nixpkgsAllowedPredicatesModule
+    nixpkgsPredicatesModule
   ];
 
   flake.modules.flake = {
     nixpkgs = nixpkgsModule;
-    nixpkgsAllowedPredicates = nixpkgsAllowedPredicatesModule;
+    nixpkgsPredicates = nixpkgsPredicatesModule;
   };
 
   perSystem = {
     nixpkgs = {
-      overlays = [ self.overlays.default ];
+      config = {
+        allowNonSource = false;
+      };
+      overlays = [
+        self.overlays.default
+      ];
+      predicates = {
+        allowNonSource =
+          p:
+          elem (getName p) [
+            # keep-sorted start
+            "cargo-bootstrap"
+            "go"
+            "rustc-bootstrap"
+            "rustc-bootstrap-wrapper"
+            # keep-sorted end
+          ];
+      };
     };
   };
 }
