b46266561a3bb11c0700f5e979f1e4e8ff062339
Lu Wang <rebmit@rebmit.moe>
2025-11-01 22:25:52 +0800

    nixpkgs: merge nixpkgsPredicatesModule into nixpkgsModule

--- modules/features/nixpkgs.nix
+++ modules/features/nixpkgs.nix
@@ -12,6 +12,18 @@ let
   inherit (lib.options) mkOption;
   inherit (flake-parts-lib) mkPerSystemOption;
 
+  predicateOption = mkOption {
+    type = (types.functionTo types.bool) // {
+      merge =
+        _loc: defs:
+        let
+          funcs = map (d: d.value) defs;
+        in
+        foldl (acc: f: acc ++ [ f ]) [ ] funcs;
+    };
+    default = _: false;
+  };
+
   nixpkgsModule = _: {
     options.perSystem = mkPerSystemOption (
       {
@@ -69,6 +81,31 @@ let
               List of overlays to apply to target packages only.
             '';
           };
+          predicates = {
+            allowUnfree = predicateOption // {
+              description = ''
+                List of predicates deciding which packages are allowed even if unfree.
+                Only effective when `allowUnfree = false`.
+              '';
+            };
+            allowNonSource = predicateOption // {
+              description = ''
+                List of predicates deciding which packages are allowed even if they are not built from source.
+                Only effective when `allowNonSource = false`.
+              '';
+            };
+            allowBroken = predicateOption // {
+              description = ''
+                List of predicates deciding which packages are allowed even if marked as broken.
+                Only effective when `allowBroken = false`.
+              '';
+            };
+            allowInsecure = predicateOption // {
+              description = ''
+                List of predicates deciding which packages are allowed even if marked as insecure.
+              '';
+            };
+          };
         };
 
         config = {
@@ -81,64 +118,12 @@ let
               crossOverlays
               ;
           };
-        };
-      }
-    );
-  };
 
-  nixpkgsPredicatesModule = _: {
-    options.perSystem = mkPerSystemOption (
-      { config, ... }:
-      let
-        predicateOption = mkOption {
-          type = (types.functionTo types.bool) // {
-            merge =
-              _loc: defs:
-              let
-                funcs = map (d: d.value) defs;
-              in
-              foldl (acc: f: acc ++ [ f ]) [ ] funcs;
-          };
-          default = _: false;
-        };
-
-        cfg = config.nixpkgs.predicates;
-      in
-      {
-        _file = ./nixpkgs.nix;
-
-        options.nixpkgs.predicates = {
-          allowUnfree = predicateOption // {
-            description = ''
-              List of predicates deciding which packages are allowed even if unfree.
-              Only effective when `allowUnfree = false`.
-            '';
-          };
-          allowNonSource = predicateOption // {
-            description = ''
-              List of predicates deciding which packages are allowed even if they are not built from source.
-              Only effective when `allowNonSource = false`.
-            '';
-          };
-          allowBroken = predicateOption // {
-            description = ''
-              List of predicates deciding which packages are allowed even if marked as broken.
-              Only effective when `allowBroken = false`.
-            '';
-          };
-          allowInsecure = predicateOption // {
-            description = ''
-              List of predicates deciding which packages are allowed even if marked as insecure.
-            '';
-          };
-        };
-
-        config = {
           nixpkgs.config = {
-            allowUnfreePredicate = p: any (f: f p) cfg.allowUnfree;
-            allowNonSourcePredicate = p: any (f: f p) cfg.allowNonSource;
-            allowBrokenPredicate = p: any (f: f p) cfg.allowBroken;
-            allowInsecurePredicate = p: any (f: f p) cfg.allowInsecure;
+            allowUnfreePredicate = p: any (f: f p) cfg.predicates.allowUnfree;
+            allowNonSourcePredicate = p: any (f: f p) cfg.predicates.allowNonSource;
+            allowBrokenPredicate = p: any (f: f p) cfg.predicates.allowBroken;
+            allowInsecurePredicate = p: any (f: f p) cfg.predicates.allowInsecure;
           };
         };
       }
@@ -146,13 +131,7 @@ let
   };
 in
 {
-  imports = [
-    nixpkgsModule
-    nixpkgsPredicatesModule
-  ];
+  imports = [ nixpkgsModule ];
 
-  flake.flakeModules = {
-    nixpkgs = nixpkgsModule;
-    nixpkgsPredicates = nixpkgsPredicatesModule;
-  };
+  flake.flakeModules.nixpkgs = nixpkgsModule;
 }
