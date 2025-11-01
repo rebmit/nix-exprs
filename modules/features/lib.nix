b3820385c4651ea4a4163d5e6b5b86af83bb4337
Lu Wang <rebmit@rebmit.moe>
2025-11-02 00:26:04 +0800

    treewide: expose flakeModules.{flakeModules,lib}

---
+++ modules/features/lib.nix
@@ -0,0 +1,43 @@
+{
+  config,
+  lib,
+  flake-parts-lib,
+  ...
+}:
+let
+  inherit (lib) types;
+  inherit (lib.attrsets) recursiveUpdate;
+  inherit (lib.fixedPoints) makeExtensible;
+  inherit (lib.lists) foldl;
+  inherit (lib.options) mkOption;
+  inherit (flake-parts-lib) mkSubmoduleOptions;
+
+  libModule = _: {
+    options.flake = mkSubmoduleOptions {
+      lib = mkOption {
+        type = types.functionTo (types.attrsOf types.unspecified) // {
+          merge =
+            _loc: defs:
+            let
+              funcs = map (d: d.value) defs;
+            in
+            self: foldl (acc: f: recursiveUpdate acc (f self)) { } funcs;
+        };
+        default = _: { };
+        apply = makeExtensible;
+        description = ''
+          An extensible set of library functions provided by the flake.
+        '';
+      };
+    };
+
+    config = {
+      _module.args.selfLib = config.flake.lib;
+    };
+  };
+in
+{
+  imports = [ libModule ];
+
+  flake.flakeModules.lib = libModule;
+}
