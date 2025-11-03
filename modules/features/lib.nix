d7b62f5d04f57e817ee2dc67d5fa14613be8d70d
Lu Wang <rebmit@rebmit.moe>
2025-11-03 20:03:17 +0800

    unify: init

--- modules/features/lib.nix
+++ modules/features/lib.nix
@@ -1,5 +1,4 @@
 {
-  config,
   lib,
   flake-parts-lib,
   ...
@@ -9,32 +8,35 @@ let
   inherit (lib.attrsets) recursiveUpdate;
   inherit (lib.fixedPoints) makeExtensible;
   inherit (lib.lists) foldl;
+  inherit (lib.modules) mkDefault;
   inherit (lib.options) mkOption;
   inherit (flake-parts-lib) mkSubmoduleOptions;
 
-  libModule = _: {
-    options.flake = mkSubmoduleOptions {
-      lib = mkOption {
-        type = types.functionTo (types.attrsOf types.unspecified) // {
-          merge =
-            _loc: defs:
-            let
-              funcs = map (d: d.value) defs;
-            in
-            self: foldl (acc: f: recursiveUpdate acc (f self)) { } funcs;
+  libModule =
+    { config, ... }:
+    {
+      options.flake = mkSubmoduleOptions {
+        lib = mkOption {
+          type = types.functionTo (types.attrsOf types.unspecified) // {
+            merge =
+              _loc: defs:
+              let
+                funcs = map (d: d.value) defs;
+              in
+              self: foldl (acc: f: recursiveUpdate acc (f self)) { } funcs;
+          };
+          default = _: { };
+          apply = makeExtensible;
+          description = ''
+            An extensible set of library functions provided by the flake.
+          '';
         };
-        default = _: { };
-        apply = makeExtensible;
-        description = ''
-          An extensible set of library functions provided by the flake.
-        '';
+      };
+
+      config = {
+        _module.args.selfLib = mkDefault config.flake.lib;
       };
     };
-
-    config = {
-      _module.args.selfLib = config.flake.lib;
-    };
-  };
 in
 {
   imports = [ libModule ];
