93f1b35933a6a0e71172c050fd88e34d8fc57e50
Lu Wang <rebmit@rebmit.moe>
2025-11-09 20:15:09 +0800

    services/pipewire: migrate from nixos-config

---
+++ profiles/services/pipewire.nix
@@ -0,0 +1,25 @@
+{
+  unify.modules."services/pipewire" = {
+    nixos = {
+      meta = {
+        tags = [ "multimedia" ];
+        requires = [
+          "external/preservation"
+          "security/rtkit"
+        ];
+      };
+
+      module = _: {
+        services.pipewire = {
+          enable = true;
+          alsa.enable = true;
+          jack.enable = true;
+          pulse.enable = true;
+          systemWide = true;
+        };
+
+        preservation.directories = [ "/var/lib/pipewire" ];
+      };
+    };
+  };
+}
