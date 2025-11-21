0a0a645f1cad2f0ed5cba456622a7ef42d9d4007
Lu Wang <rebmit@rebmit.moe>
2025-11-21 20:56:52 +0800

    programs/delta: enable line numbers and navigation

--- profiles/programs/delta.nix
+++ profiles/programs/delta.nix
@@ -7,6 +7,10 @@
           programs.delta = {
             enable = true;
             enableGitIntegration = true;
+            options = {
+              line-numbers = true;
+              navigate = true;
+            };
           };
         };
     };
