d16b379d64569113811d36533479ce4dce0fdddb
Lu Wang <rebmit@rebmit.moe>
2025-11-22 15:33:06 +0800

    nix/registry: use `nix.nixPath` as NIX_PATH is never unset

--- profiles/nix/registry.nix
+++ profiles/nix/registry.nix
@@ -13,17 +13,13 @@
         {
           nixpkgs.flake = {
             setFlakeRegistry = true;
-            setNixPath = false;
+            setNixPath = true;
           };
 
           nix = {
             registry.p.flake = self;
             settings = {
               flake-registry = "/etc/nix/registry.json";
-              nix-path = [
-                "nixpkgs=flake:nixpkgs"
-                "p=flake:p"
-              ];
             };
           };
         };
