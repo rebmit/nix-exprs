7236c887031194471b131deef84976f63bda4b19
Lu Wang <rebmit@rebmit.moe>
2025-12-04 02:36:47 +0800

    treewide: drop direct nixpkgs input from flake partitions

--- configs/_flake/flake.nix
+++ configs/_flake/flake.nix
@@ -1,37 +1,33 @@
 {
   inputs = {
-    # nixpkgs
-
-    nixpkgs.follows = "rebmit/nixpkgs";
-
     # nixos modules
 
     home-manager = {
       url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "rebmit/nixpkgs";
     };
     sops-nix = {
       url = "github:Mic92/sops-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "rebmit/nixpkgs";
     };
     disko = {
       url = "github:nix-community/disko";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "rebmit/nixpkgs";
     };
 
     # darwin modules
 
     nix-darwin = {
       url = "github:nix-darwin/nix-darwin";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "rebmit/nixpkgs";
     };
 
     # programs
 
     niri-flake = {
       url = "github:sodiboo/niri-flake";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.nixpkgs-stable.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "rebmit/nixpkgs";
+      inputs.nixpkgs-stable.follows = "rebmit/nixpkgs";
     };
 
     # libraries
@@ -40,7 +36,7 @@
     flake-utils.url = "github:numtide/flake-utils";
     dns = {
       url = "github:nix-community/dns.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "rebmit/nixpkgs";
       inputs.flake-utils.follows = "flake-utils";
     };
   };
--- dev/_flake/flake.nix
+++ dev/_flake/flake.nix
@@ -1,31 +1,27 @@
 {
   inputs = {
-    # nixpkgs
-
-    nixpkgs.follows = "rebmit/nixpkgs";
-
     # flake modules
 
     devshell = {
       url = "github:numtide/devshell";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "rebmit/nixpkgs";
     };
     git-hooks-nix = {
       url = "github:cachix/git-hooks.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "rebmit/nixpkgs";
       inputs.gitignore.follows = "gitignore-nix";
       inputs.flake-compat.follows = "flake-compat";
     };
     treefmt-nix = {
       url = "github:numtide/treefmt-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "rebmit/nixpkgs";
     };
 
     # libraries
 
     gitignore-nix = {
       url = "github:hercules-ci/gitignore.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "rebmit/nixpkgs";
     };
     rebmit.url = ./../..;
 
@@ -33,7 +29,7 @@
 
     nixpkgs-terraform-providers-bin = {
       url = "github:nix-community/nixpkgs-terraform-providers-bin";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "rebmit/nixpkgs";
     };
 
     # misc
