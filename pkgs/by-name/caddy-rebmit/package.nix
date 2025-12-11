909c4dbf523510a67c5345af6466b26f3b1dc47e
Lu Wang <rebmit@rebmit.moe>
2025-12-11 08:38:52 +0800

    caddy-rebmit: init

---
+++ pkgs/by-name/caddy-rebmit/package.nix
@@ -0,0 +1,17 @@
+let
+  caddy-rebmit =
+    { caddy }:
+    caddy.withPlugins {
+      hash = "sha256-ujjlyTBJqrEoSFDxb14rWh7VDCaSmhqnC+/BqYgbMgY=";
+      plugins = [
+        "github.com/mholt/caddy-l4@v0.0.0-20251209130418-1a3490ef786a"
+      ];
+    };
+in
+{
+  perSystem =
+    { prev, ... }:
+    {
+      packages.caddy-rebmit = prev.callPackage caddy-rebmit { };
+    };
+}
