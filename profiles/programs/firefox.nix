14488bfa7a663634f87c555a5c1cd74f4605ef17
Lu Wang <rebmit@rebmit.moe>
2025-12-12 11:35:17 +0800

    programs/firefox: migrate from nixos-config

---
+++ profiles/programs/firefox.nix
@@ -0,0 +1,164 @@
+# Portions of this file are sourced from
+# https://github.com/NickCao/flakes/blob/3b03efb676ea602575c916b2b8bc9d9cd13b0d85/nixos/mainframe/home.nix (MIT License)
+# https://github.com/llakala/nixos/blob/b3c5fbde5a5f78c91ee658250f9b42418b73a7b7/apps/gui/firefox.nix (MIT License)
+{ lib, ... }:
+let
+  inherit (lib.attrsets) attrsToList;
+  inherit (lib.lists) singleton;
+in
+{
+  flake.unify.modules."programs/firefox" = {
+    homeManager = {
+      meta = {
+        requires = [ "external/preservation" ];
+      };
+
+      module =
+        { pkgs, ... }:
+        {
+          programs.firefox.enable = true;
+
+          programs.firefox.policies = {
+            AutofillAddressEnabled = false;
+            AutofillCreditCardEnabled = false;
+            DisableAccounts = true;
+            DisableFirefoxAccounts = true;
+            DisableFirefoxStudies = true;
+            DisablePocket = true;
+            DisableTelemetry = true;
+            EnableTrackingProtection = {
+              Value = true;
+              Locked = true;
+              Cryptomining = true;
+              Fingerprinting = true;
+              EmailTracking = true;
+            };
+            FirefoxHome = {
+              Search = true;
+              TopSites = false;
+              SponsoredTopSites = false;
+              Highlights = false;
+              Pocket = false;
+              SponsoredPocket = false;
+              Snippets = false;
+              Locked = true;
+            };
+            FirefoxSuggest = {
+              WebSuggestions = false;
+              SponsoredSuggestions = false;
+              ImproveSuggest = false;
+              Locked = true;
+            };
+            PasswordManagerEnabled = false;
+            PostQuantumKeyAgreementEnabled = true;
+            SearchSuggestEnabled = false;
+          };
+
+          programs.firefox.policies.ExtensionSettings = {
+            "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+              installation_mode = "force_installed";
+              install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
+            };
+            "uBlock0@raymondhill.net" = {
+              installation_mode = "force_installed";
+              install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+            };
+            "addon@darkreader.org" = {
+              installation_mode = "force_installed";
+              install_url = "https://addons.mozilla.org/firefox/downloads/latest/darkreader/latest.xpi";
+            };
+            "zotero@chnm.gmu.edu" = {
+              installation_mode = "force_installed";
+              install_url = "https://www.zotero.org/download/connector/dl?browser=firefox";
+            };
+            "firefox@tampermonkey.net" = {
+              installation_mode = "force_installed";
+              install_url = "https://addons.mozilla.org/firefox/downloads/latest/tampermonkey/latest.xpi";
+            };
+            "{3c078156-979c-498b-8990-85f7987dd929}" = {
+              installation_mode = "force_installed";
+              install_url = "https://addons.mozilla.org/firefox/downloads/latest/sidebery/latest.xpi";
+            };
+          };
+
+          programs.firefox.policies.Preferences = {
+            "browser.urlbar.autoFill.adaptiveHistory.enabled" = true;
+            "browser.tabs.closeWindowWithLastTab" = false;
+            "browser.tabs.inTitlebar" = 0;
+            "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+            "svg.context-properties.content.enabled" = true;
+            "browser.theme.dark-private-windows" = false;
+            "widget.gtk.rounded-bottom-corners.enabled" = true;
+          };
+
+          programs.firefox.profiles.default = {
+            isDefault = true;
+            search = {
+              force = true;
+              default = "google";
+            };
+            containersForce = true;
+            containers = {
+              "Underlay" = {
+                id = 1;
+                color = "green";
+                icon = "fence";
+              };
+            };
+            userChrome = ''
+              @import "${pkgs.firefox-gnome-theme}/lib/firefox-gnome-theme/userChrome.css";
+
+              #TabsToolbar {
+                display: none;
+              }
+            '';
+            userContent = ''
+              @import "${pkgs.firefox-gnome-theme}/lib/firefox-gnome-theme/userContent.css";
+            '';
+          };
+
+          programs.firefox.profiles.default.search.engines = {
+            "bing".metaData.hidden = true;
+            "ebay".metaData.hidden = true;
+            "amazondotcom-us".metaData.hidden = true;
+            "wikipedia".metaData.hidden = true;
+            "Nixpkgs" = {
+              urls = singleton {
+                template = "https://search.nixos.org/packages";
+                params = attrsToList {
+                  "channel" = "unstable";
+                  "query" = "{searchTerms}";
+                };
+              };
+              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              definedAliases = [ "@np" ];
+            };
+            "NixOS Options" = {
+              urls = singleton {
+                template = "https://search.nixos.org/options";
+                params = attrsToList {
+                  "channel" = "unstable";
+                  "query" = "{searchTerms}";
+                };
+              };
+              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              definedAliases = [ "@no" ];
+            };
+            "Home Manager Options" = {
+              urls = singleton {
+                template = "https://home-manager-options.extranix.com";
+                params = attrsToList {
+                  "release" = "master";
+                  "query" = "{searchTerms}";
+                };
+              };
+              icon = "https://home-manager-options.extranix.com/images/favicon.png";
+              definedAliases = [ "@ho" ];
+            };
+          };
+
+          preservation.preserveAt.state.directories = [ ".mozilla" ];
+        };
+    };
+  };
+}
