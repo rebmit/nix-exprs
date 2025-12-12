a71add15acb1c25b12827e2233dc728c312223d0
Lu Wang <rebmit@rebmit.moe>
2025-12-12 11:37:14 +0800

    services/user/darkman: migrate from nixos-config

---
+++ profiles/services/user/darkman.nix
@@ -0,0 +1,50 @@
+{
+  flake.unify.modules."services/user/darkman" = {
+    homeManager = {
+      meta = {
+        requires = [ "external/preservation" ];
+      };
+
+      module =
+        {
+          config,
+          pkgs,
+          ...
+        }@hm:
+        let
+          defaultMode = pkgs.writeText "darkman-default-mode" "light";
+        in
+        {
+          services.darkman.enable = true;
+
+          preservation.preserveAt.state.directories = [ ".cache/darkman" ];
+
+          systemd.user.tmpfiles.rules = [
+            "C %h/.cache/darkman/mode.txt - - - - ${defaultMode}"
+            "z %h/.cache/darkman/mode.txt 644 - - -"
+          ];
+
+          home.packages = with pkgs; [
+            (writeShellApplication {
+              name = "toggle-theme";
+              runtimeInputs = [ config.services.darkman.package ];
+              text = ''
+                darkman toggle
+              '';
+            })
+          ];
+
+          home.activation.restartDarkman = hm.lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+            if ${config.systemd.user.systemctlPath} --user is-active darkman; then
+              ${config.systemd.user.systemctlPath} --user restart darkman
+            fi
+          '';
+
+          systemd.user.services.darkman.Unit = {
+            After = [ "graphical-session.target" ];
+            Requisite = [ "graphical-session.target" ];
+          };
+        };
+    };
+  };
+}
