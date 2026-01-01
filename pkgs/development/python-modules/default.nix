6e26e966b5250cae2445ef9bbd4b151638ca74a5
Lu Wang <rebmit@rebmit.moe>
2026-01-02 02:13:09 +0800

    python3Packages.pyscard_2_2_1: init

---
+++ pkgs/development/python-modules/default.nix
@@ -0,0 +1,36 @@
+{ config, lib, ... }:
+let
+  inherit (lib.attrsets) genAttrs;
+  inherit (lib.fixedPoints) composeManyExtensions;
+in
+{
+  overlays.default =
+    { prev, ... }:
+    genAttrs
+      [
+        "python27"
+        "python310"
+        "python311"
+        "python312"
+        "python313"
+        "python314"
+        "python315"
+        "python3Minimal"
+        "pypy27"
+        "pypy310"
+        "pypy311"
+      ]
+      (
+        name:
+        prev.${name}.override (
+          prev:
+          prev
+          // {
+            packageOverrides = composeManyExtensions [
+              (prev.packageOverrides or (_: _: { }))
+              (_: config.overlays.python _)
+            ];
+          }
+        )
+      );
+}
