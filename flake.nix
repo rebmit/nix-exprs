2aa4d0cc2da40db12b3a0bf2d39b680e32373732
Lu Wang <rebmit@rebmit.moe>
2026-01-02 17:12:38 +0800

    flake: evaluate flake-parts modules directly with evalModules

--- flake.nix
+++ flake.nix
@@ -20,6 +20,7 @@
 
   outputs =
     inputs@{
+      self,
       flake-parts,
       nixpkgs,
       import-tree,
@@ -34,97 +35,105 @@
         listToAttrs
         ;
       inherit (lib.lists) foldl;
-      inherit (lib.modules) mkDefault;
+      inherit (lib.modules) evalModules mkOptionDefault;
       inherit (lib.strings) splitString;
       inherit (lib.trivial) pipe;
     in
-    flake-parts.lib.mkFlake { inherit inputs; } (
-      { config, partitionStack, ... }:
-      {
-        systems = [
-          "x86_64-linux"
-          "aarch64-linux"
-          "aarch64-darwin"
-        ];
+    (evalModules {
+      specialArgs = {
+        inherit inputs self;
+        flake-parts-lib = flake-parts.lib;
+        moduleLocation = "${self.outPath}/flake.nix";
+        partitionStack = [ ];
+      };
 
-        disabledModules = [ "${flake-parts}/all-modules.nix" ];
+      class = "flake";
 
-        imports = [
-          # keep-sorted start block=yes
-          "${flake-parts}/modules/flake.nix"
-          "${flake-parts}/modules/moduleWithSystem.nix"
-          "${flake-parts}/modules/perSystem.nix"
-          "${flake-parts}/modules/transposition.nix"
-          "${flake-parts}/modules/withSystem.nix"
-          flake-parts.flakeModules.partitions
-          # keep-sorted end
-        ];
+      modules = [
+        # keep-sorted start
+        "${flake-parts}/modules/flake.nix"
+        "${flake-parts}/modules/moduleWithSystem.nix"
+        "${flake-parts}/modules/perSystem.nix"
+        "${flake-parts}/modules/transposition.nix"
+        "${flake-parts}/modules/withSystem.nix"
+        flake-parts.flakeModules.partitions
+        # keep-sorted end
 
-        transposition = mkDefault { };
-
-        partitions = optionalAttrs (partitionStack == [ ]) {
-          # keep-sorted start block=yes
-          common.module = import-tree ./common;
-          configs = {
-            extraInputsFlake = ./configs/_flake;
-            module = import-tree ./configs;
-          };
-          dev = {
-            extraInputsFlake = ./dev/_flake;
-            module = import-tree ./dev;
-          };
-          lib.module = import-tree ./lib;
-          modules.module = import-tree ./modules;
-          pkgs.module = import-tree ./pkgs;
-          profiles.module = import-tree ./profiles;
-          # keep-sorted end
-        };
-
-        flake = optionalAttrs (partitionStack == [ ]) (
-          let
-            partitionAttr =
-              partition: attrPath:
-              (getAttrFromPath (splitString "/" attrPath) config.partitions.${partition}.module.flake);
-          in
+        (
+          { config, partitionStack, ... }:
           {
-            # keep-sorted start block=yes
-            checks =
-              foldl
-                (
-                  acc: v:
-                  pipe config.systems [
-                    (map (system: nameValuePair system (acc.${system} or { } // v.${system} or { })))
-                    listToAttrs
-                  ]
-                )
-                { }
-                [
-                  # keep-sorted start
-                  (partitionAttr "configs" "checks")
-                  (partitionAttr "modules" "checks")
-                  (partitionAttr "pkgs" "checks")
-                  (partitionAttr "profiles" "checks")
-                  # keep-sorted end
-                ];
-            devShells = partitionAttr "dev" "devShells";
-            flakeModules = partitionAttr "modules" "flakeModules";
-            formatter = partitionAttr "dev" "formatter";
-            legacyPackages = partitionAttr "pkgs" "legacyPackages";
-            lib = partitionAttr "lib" "lib";
-            nixosConfigurations = partitionAttr "configs" "nixosConfigurations";
-            nixosModules = partitionAttr "modules" "nixosModules";
-            overlays = partitionAttr "pkgs" "overlays";
-            partitions = config.partitions;
-            unify = {
+            transposition = mkOptionDefault { };
+
+            systems = [
+              "x86_64-linux"
+              "aarch64-linux"
+              "aarch64-darwin"
+            ];
+
+            partitions = optionalAttrs (partitionStack == [ ]) {
               # keep-sorted start block=yes
-              configs = partitionAttr "configs" "unify/configs";
-              lib = partitionAttr "profiles" "unify/lib";
-              modules = partitionAttr "profiles" "unify/modules";
+              configs = {
+                extraInputsFlake = ./configs/_flake;
+                module = import-tree ./configs;
+              };
+              dev = {
+                extraInputsFlake = ./dev/_flake;
+                module = import-tree ./dev;
+              };
+              lib.module = import-tree ./lib;
+              modules.module = import-tree ./modules;
+              pkgs.module = import-tree ./pkgs;
+              profiles.module = import-tree ./profiles;
               # keep-sorted end
             };
-            # keep-sorted end
+
+            flake = optionalAttrs (partitionStack == [ ]) (
+              let
+                partitionAttr =
+                  partition: attrPath:
+                  (getAttrFromPath (splitString "/" attrPath) config.partitions.${partition}.module.flake);
+              in
+              {
+                # keep-sorted start block=yes
+                checks =
+                  foldl
+                    (
+                      acc: v:
+                      pipe config.systems [
+                        (map (system: nameValuePair system (acc.${system} or { } // v.${system} or { })))
+                        listToAttrs
+                      ]
+                    )
+                    { }
+                    [
+                      # keep-sorted start
+                      (partitionAttr "configs" "checks")
+                      (partitionAttr "modules" "checks")
+                      (partitionAttr "pkgs" "checks")
+                      (partitionAttr "profiles" "checks")
+                      # keep-sorted end
+                    ];
+                devShells = partitionAttr "dev" "devShells";
+                flakeModules = partitionAttr "modules" "flakeModules";
+                formatter = partitionAttr "dev" "formatter";
+                legacyPackages = partitionAttr "pkgs" "legacyPackages";
+                lib = partitionAttr "lib" "lib";
+                nixosConfigurations = partitionAttr "configs" "nixosConfigurations";
+                nixosModules = partitionAttr "modules" "nixosModules";
+                overlays = partitionAttr "pkgs" "overlays";
+                partitions = config.partitions;
+                unify = {
+                  # keep-sorted start block=yes
+                  configs = partitionAttr "configs" "unify/configs";
+                  lib = partitionAttr "profiles" "unify/lib";
+                  modules = partitionAttr "profiles" "unify/modules";
+                  # keep-sorted end
+                };
+                # keep-sorted end
+              }
+            );
           }
-        );
-      }
-    );
+        )
+      ];
+    }).config.flake;
 }
