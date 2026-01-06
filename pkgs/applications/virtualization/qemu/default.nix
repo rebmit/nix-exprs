89adb24b962fedf422fcf3d1947bb386c8c5d526
Lu Wang <rebmit@rebmit.moe>
2026-01-06 23:27:14 +0800

    qemu: init at 10.2.0-unstable-2026-01-06

---
+++ pkgs/applications/virtualization/qemu/default.nix
@@ -0,0 +1,36 @@
+{ lib, ... }:
+let
+  inherit (lib.meta) getExe';
+in
+{
+  overlays.default =
+    { final, prev, ... }:
+    {
+      qemu = prev.qemu.overrideAttrs {
+        version = "10.2.0-unstable-2026-01-06";
+        src = final.fetchFromGitHub {
+          owner = "rebmit";
+          repo = "qemu";
+          rev = "3ed318f65253c24c380ff805a8c326bcace0b251";
+          fetchSubmodules = true;
+          hash = "sha256-s3nHA1Wl8nDduaiJimKBTMaxSUY7UTUxj5Y2ufSiLRI=";
+          postFetch = ''
+            cd $out
+            subprojects="keycodemapdb libvfio-user berkeley-softfloat-3 berkeley-testfloat-3"
+            for sp in $subprojects; do
+              ${getExe' prev.meson "meson"} subprojects download $sp
+            done
+            rm -r subprojects/*/.git
+          '';
+        };
+      };
+    };
+
+  checks =
+    { pkgs, ... }:
+    {
+      packages = {
+        inherit (pkgs) qemu_kvm;
+      };
+    };
+}
