63bfe06fd02d2dc52a1dd2142adb6d42dc954e7b
Lu Wang <rebmit@rebmit.moe>
2026-01-26 00:19:50 +0800

    profiles/nixos/kernel/{latest,testing}: init

---
+++ modules/profiles/system/nixos/kernel/latest.nix
@@ -0,0 +1,9 @@
+{
+  unify.profiles.system._.nixos._.kernel._.latest = {
+    nixos =
+      { pkgs, ... }:
+      {
+        boot.kernelPackages = pkgs.linuxPackages_latest;
+      };
+  };
+}
---
+++ modules/profiles/system/nixos/kernel/testing.nix
@@ -0,0 +1,9 @@
+{
+  unify.profiles.system._.nixos._.kernel._.testing = {
+    nixos =
+      { pkgs, ... }:
+      {
+        boot.kernelPackages = pkgs.linuxPackages_testing;
+      };
+  };
+}
