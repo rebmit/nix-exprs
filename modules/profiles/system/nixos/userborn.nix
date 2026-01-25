aae8ae318f65533d7d11d6b9e5fb670ec482d37a
Lu Wang <rebmit@rebmit.moe>
2026-01-26 00:25:21 +0800

    profiles/nixos/userborn: init

---
+++ modules/profiles/system/nixos/userborn.nix
@@ -0,0 +1,17 @@
+{
+  unify.profiles.system._.nixos._.userborn = {
+    nixos =
+      { ... }:
+      {
+        users = {
+          mutableUsers = false;
+          users.root.createHome = true;
+        };
+
+        services.userborn = {
+          enable = true;
+          passwordFilesLocation = "/var/lib/nixos/userborn";
+        };
+      };
+  };
+}
