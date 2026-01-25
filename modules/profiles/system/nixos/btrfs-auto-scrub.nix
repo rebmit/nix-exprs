30dc464158ae132d8c902b41e974cc91c074e5c5
Lu Wang <rebmit@rebmit.moe>
2026-01-26 02:42:10 +0800

    profiles/nixos/btrfs-auto-scrub: init

---
+++ modules/profiles/system/nixos/btrfs-auto-scrub.nix
@@ -0,0 +1,28 @@
+{ lib, ... }:
+let
+  inherit (lib.modules) mkVMOverride;
+in
+{
+  unify.profiles.system._.nixos._.btrfs-auto-scrub = {
+    requires = [ "features/preservation" ];
+
+    nixos =
+      { ... }:
+      {
+        services.btrfs.autoScrub.enable = true;
+
+        preservation.preserveAt.state.directories = [
+          {
+            directory = "/var/lib/btrfs";
+            mode = "0700";
+            user = "root";
+            group = "root";
+          }
+        ];
+
+        virtualisation.vmVariant = {
+          services.btrfs.autoScrub.enable = mkVMOverride false;
+        };
+      };
+  };
+}
