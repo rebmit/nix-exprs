f755823718c6a31e60d3204373a2e5886c85a7b7
Lu Wang <rebmit@rebmit.moe>
2026-01-28 00:09:04 +0800

    profiles/git: init

---
+++ modules/profiles/programs/git/user.nix
@@ -0,0 +1,26 @@
+{
+  unify.profiles.programs._.git._.user =
+    { ... }:
+    {
+      homeManager =
+        { ... }:
+        {
+          programs.git = {
+            enable = true;
+            lfs.enable = true;
+            settings = {
+              commit.verbose = true;
+              diff.algorithm = "patience";
+              fetch.prune = true;
+              init.defaultBranch = "master";
+              merge.conflictStyle = "zdiff3";
+              pull.rebase = true;
+              rebase = {
+                autoSquash = true;
+                autoStash = true;
+              };
+            };
+          };
+        };
+    };
+}
