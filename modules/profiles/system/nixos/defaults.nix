532a96c3b77708b8b0060e647a8a01249a855f1c
Lu Wang <rebmit@rebmit.moe>
2026-01-28 00:29:34 +0800

    profiles/nixos/defaults: init

---
+++ modules/profiles/system/nixos/defaults.nix
@@ -0,0 +1,36 @@
+{ lib, ... }:
+let
+  inherit (lib.modules) mkDefault;
+in
+{
+  unify.profiles.system._.nixos._.defaults =
+    { host, ... }:
+    {
+      contexts = [ "host" ];
+
+      nixos =
+        { ... }:
+        {
+          boot.tmp.useTmpfs = mkDefault true;
+
+          environment = {
+            defaultPackages = mkDefault [ ];
+            stub-ld.enable = mkDefault false;
+          };
+
+          networking = {
+            domain = mkDefault "rebmit.link";
+            hostName = mkDefault host.hostName;
+            nftables.enable = mkDefault true;
+            useNetworkd = mkDefault true;
+            useDHCP = mkDefault false;
+          };
+
+          services.dbus.implementation = mkDefault "broker";
+
+          time.timeZone = mkDefault "Asia/Hong_Kong";
+
+          users.mutableUsers = mkDefault false;
+        };
+    };
+}
