c6e05ca1c419e8f12624b4ee0ecd06b84afceb7f
Lu Wang <rebmit@rebmit.moe>
2026-01-31 01:48:28 +0800

    profiles/networking/tools/base: init

---
+++ modules/profiles/networking/tools/base.nix
@@ -0,0 +1,23 @@
+{
+  unify.profiles.networking._.tools._.base =
+    { ... }:
+    {
+      nixos =
+        { pkgs, ... }:
+        {
+          programs.mtr.enable = true;
+
+          environment.systemPackages = with pkgs; [
+            # keep-sorted start
+            dnsutils
+            ethtool
+            iperf3
+            netcat
+            nmap
+            socat
+            tcpdump
+            # keep-sorted end
+          ];
+        };
+    };
+}
