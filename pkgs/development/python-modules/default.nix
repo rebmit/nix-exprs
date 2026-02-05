8b87c1241549c4f96b287116ca147f204ab46759
Lu Wang <rebmit@rebmit.moe>
2026-02-05 20:13:24 +0800

    pkgs/python-modules: switch to pythonPackagesExtensions

--- pkgs/development/python-modules/default.nix
+++ pkgs/development/python-modules/default.nix
@@ -1,31 +1,10 @@
-{ config, lib, ... }:
-let
-  inherit (lib.attrsets) genAttrs;
-  inherit (lib.fixedPoints) composeManyExtensions;
-in
+{ config, ... }:
 {
   overlays.default =
     { prev, ... }:
-    genAttrs
-      [
-        "python310"
-        "python311"
-        "python312"
-        "python313"
-        "python314"
-        "python315"
-        "python3Minimal"
-        "pypy27"
-        "pypy310"
-        "pypy311"
-      ]
-      (
-        name:
-        prev.${name}.override (prev: {
-          packageOverrides = composeManyExtensions [
-            (prev.packageOverrides or (_: _: { }))
-            config.overlays.python
-          ];
-        })
-      );
+    {
+      pythonPackagesExtensions = (prev.pythonPackagesExtensions or [ ]) ++ [
+        config.overlays.python
+      ];
+    };
 }
