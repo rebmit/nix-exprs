58460459852d139b5e24bbee22c0def98c01a4a8
Lu Wang <rebmit@rebmit.moe>
2026-02-12 20:16:34 +0800

    profiles/yazi: init

---
+++ modules/profiles/programs/yazi/user.nix
@@ -0,0 +1,25 @@
+{
+  unify.profiles.programs._.yazi._.user =
+    { ... }:
+    {
+      homeManager =
+        { ... }:
+        {
+          programs.yazi = {
+            enable = true;
+            shellWrapperName = "ra";
+            settings = {
+              mgr = {
+                sort_by = "natural";
+                linemode = "size";
+              };
+              preview = {
+                tab_size = 2;
+                max_width = 1000;
+                max_height = 1000;
+              };
+            };
+          };
+        };
+    };
+}
