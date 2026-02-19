914c0860439bba5ceeb7699e2dc4e64182c0e6c3
Lu Wang <rebmit@rebmit.moe>
2026-02-19 19:36:52 +0800

    profiles/mpv: init

---
+++ modules/profiles/programs/mpv/user.nix
@@ -0,0 +1,40 @@
+{ lib, ... }:
+let
+  inherit (builtins) attrValues;
+  inherit (lib.attrsets) optionalAttrs;
+  inherit (lib.lists) optionals;
+in
+{
+  unify.profiles.programs._.mpv._.user =
+    { ... }:
+    {
+      requires = [ "profiles/misc/fontconfig" ];
+
+      homeManager =
+        { pkgs, ... }:
+        {
+          home.packages = [
+            (pkgs.mpv.override {
+              # https://github.com/NixOS/nixpkgs/issues/464174
+              extraMakeWrapperArgs = optionals pkgs.stdenv.hostPlatform.isDarwin [
+                "--append-flag"
+                "--osd-font-provider=fontconfig"
+              ];
+              scripts = attrValues (
+                {
+                  inherit (pkgs.mpvScripts)
+                    # keep-sorted start
+                    modernz
+                    thumbfast
+                    # keep-sorted end
+                    ;
+                }
+                // optionalAttrs pkgs.stdenv.hostPlatform.isLinux {
+                  inherit (pkgs.mpvScripts) mpris;
+                }
+              );
+            })
+          ];
+        };
+    };
+}
