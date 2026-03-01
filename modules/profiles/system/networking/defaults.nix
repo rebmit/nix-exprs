6dee71ff7fdb6a98e8d73d787b4010a580ea0158
Lu Wang <rebmit@rebmit.moe>
2026-03-01 21:55:26 +0800

    profiles/system/networking/defaults: enable BBR and increase UDP buffer size

--- modules/profiles/system/networking/defaults.nix
+++ modules/profiles/system/networking/defaults.nix
@@ -5,6 +5,13 @@
       nixos =
         { ... }:
         {
+          boot.kernel.sysctl = {
+            "net.core.default_qdisc" = "fq";
+            "net.ipv4.tcp_congestion_control" = "bbr";
+            "net.core.rmem_max" = 7500000;
+            "net.core.wmem_max" = 7500000;
+          };
+
           networking = {
             nftables.enable = true;
             useNetworkd = true;
