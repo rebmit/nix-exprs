2eb3b9e0fd19beb309698a11cbdea6500b2bd370
Lu Wang <rebmit@rebmit.moe>
2026-04-12 04:08:49 +0800

    flake: drop import-tree

--- flake/flake.nix
+++ flake/flake.nix
@@ -40,7 +40,6 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    import-tree.url = "github:vic/import-tree";
     nix-darwin = {
       url = "github:nix-darwin/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
