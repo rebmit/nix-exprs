5f18aeb09dcc044f47dd7f1c9be7272527c387cd
Lu Wang <rebmit@rebmit.moe>
2026-04-12 03:12:25 +0800

    projects/self/targets: init

---
+++ modules/configs/projects/self/targets.nix
@@ -0,0 +1,15 @@
+{
+  configs.project =
+    { ... }:
+    {
+      targets = {
+        # keep-sorted start block=yes
+        aarch64-darwin = "arm64-apple-darwin";
+        aarch64-linux-gnu = "aarch64-unknown-linux-gnu";
+        aarch64-linux-musl = "aarch64-unknown-linux-musl";
+        x86_64-linux-gnu = "x86_64-unknown-linux-gnu";
+        x86_64-linux-musl = "x86_64-unknown-linux-musl";
+        # keep-sorted end
+      };
+    };
+}
