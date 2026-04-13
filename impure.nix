2131d88677dd10fec217e9f833a0c3d3fb057ca7
Lu Wang <rebmit@rebmit.moe>
2026-04-13 21:13:19 +0800

    impure: add entry point for current system

---
+++ impure.nix
@@ -0,0 +1,20 @@
+{
+  system ? builtins.currentSystem,
+}:
+
+let
+  target =
+    {
+      "aarch64-darwin" = "aarch64-darwin";
+      "aarch64-linux" = "aarch64-linux-gnu";
+      "x86_64-linux" = "x86_64-linux-gnu";
+    }
+    .${system};
+
+  outputs = import ./. { };
+in
+outputs.packages.${target}
+// {
+  devshells = outputs.devshells.${target};
+  formatters = outputs.formatters.${target};
+}
