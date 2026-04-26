6bb85766a536a7b666f78020f00e09bf08224ce7
Lu Wang <rebmit@rebmit.moe>
2026-04-26 18:50:17 +0800

    features/system/users: init

---
+++ modules/features/system/misc/users.nix
@@ -0,0 +1,97 @@
+{
+  inputs,
+  lib,
+  registry,
+  __findFile,
+  ...
+}@args:
+
+{ project, host, ... }:
+
+let
+  users = host.users;
+
+  homeManagerFor =
+    class:
+    let
+      hasHomeManager = user: user.config.configs.user.home.class == "homeManager";
+    in
+    lib.optionalAttrs (lib.any hasHomeManager (lib.attrValues users.users)) {
+      imports = [
+        {
+          nixos = users.homeManager.path + "/nixos";
+          darwin = users.homeManager.path + "/nix-darwin";
+        }
+        .${class}
+      ];
+
+      home-manager = {
+        useGlobalPkgs = true;
+        useUserPackages = true;
+        users = lib.mapAttrs (
+          _: user: lib.optionalAttrs (hasHomeManager user) user.config.modules.homeManager
+        ) users.users;
+      };
+    };
+
+  moduleFor = class: {
+    imports = lib.mapAttrsToList (_: user: user.config.modules.${class} or { }) users.users ++ [
+      (homeManagerFor class)
+    ];
+  };
+in
+{
+  configs.host =
+    { ... }:
+    {
+      options = {
+        users = {
+          users = lib.mkOption {
+            type = lib.types.lazyAttrsOf (
+              lib.rebmit.types.latticeSubmoduleWith {
+                includes = [
+                  <rebmit/features/user/misc/class>
+                  <rebmit/features/user/misc/version>
+
+                  {
+                    configs.user =
+                      { ... }:
+                      {
+                        home = {
+                          homeManager.path = users.homeManager.path;
+                        };
+                      };
+                  }
+                ];
+                internalConfigs = [ "user" ];
+                externalConfigs = { inherit project host; };
+                inputs = lib.removeAttrs args [
+                  "__findFile"
+                  "registry"
+                ];
+                registry = registry;
+              }
+            );
+            default = { };
+            description = ''
+              User configurations.
+            '';
+          };
+
+          homeManager = {
+            path = lib.mkOption {
+              type = lib.types.path;
+              default = inputs.home-manager;
+              description = ''
+                Path to the home-manager source tree to be imported.
+              '';
+            };
+          };
+        };
+      };
+    };
+
+  modules.darwin = moduleFor "darwin";
+
+  modules.nixos = moduleFor "nixos";
+}
