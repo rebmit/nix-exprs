895d937781154e71527d0e2a70ffafa9ab894c3b
Lu Wang <rebmit@rebmit.moe>
2026-04-26 20:19:04 +0800

    projects/self/hosts: inject common host config from project context

--- modules/configs/projects/self/hosts.nix
+++ modules/configs/projects/self/hosts.nix
@@ -1,5 +1,7 @@
 { lib, __findFile, ... }:
 
+{ modules, ... }:
+
 {
   includes = [ <rebmit/features/project/hosts> ];
 
@@ -11,7 +13,16 @@
           dir = <rebmit/configs/hosts>;
         in
         lib.mapAttrs (name: _: {
-          includes = [ (dir + "/${name}") ];
+          includes = [
+            (dir + "/${name}")
+            { configs.host = modules.host; }
+          ];
         }) (builtins.readDir dir);
+    };
+
+  modules.host =
+    { ... }:
+    {
+      networking.domain = lib.mkDefault "rebmit.link";
     };
 }
